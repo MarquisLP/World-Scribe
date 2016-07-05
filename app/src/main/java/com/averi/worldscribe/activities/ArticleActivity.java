1b3d9e9524a3ba0d957a321acc267344c70e5624
Mark Padilla <markjjpadilla@gmail.com>
2016-07-05 17:40:04 -0400

    Move common Article processes to ArticleActivity

--- app/src/main/java/com/averi/worldscribe/activities/ArticleActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/ArticleActivity.java
@@ -1,23 +1,74 @@
 package com.averi.worldscribe.activities;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import com.averi.worldscribe.Category;
+import com.averi.worldscribe.R;
+import com.averi.worldscribe.adapters.ConnectionsAdapter;
+import com.averi.worldscribe.utilities.AppPreferences;
+import com.averi.worldscribe.views.BottomBar;
 
 /**
  * Created by mark on 05/07/16.
  *
  * ArticleActivity is the superclass for each Activity that displays a certain Category of Articles.
  *
- * All subclasses must implement {@link #getLayoutResourceID} and call
- * {@link #setUpArticleCore} within onCreate.
+ * All subclasses must implement {@link #getLayoutResourceID} using the Layout Resource file
+ * corresponding to their respective Categories.
  */
 public abstract class ArticleActivity extends AppCompatActivity {
+
+    /**
+     * The BottomBar navigation View.
+     */
+    private BottomBar bottomBar;
+    /**
+     * The name of the World in which this Article exists.
+     */
+    private String worldName;
+    /**
+     * The {@link Category} this Article belongs to.
+     */
+    private Category category;
+    /**
+     * The name of the Article displayed by this Activity.
+     */
+    private String articleName;
+    /**
+     * Contains cards for all of the Article's {@link com.averi.worldscribe.Connection}s.
+     */
+    private RecyclerView connectionsList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResourceID());
+
+        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
+        Intent intent = getIntent();
+        worldName = intent.getStringExtra(AppPreferences.WORLD_NAME);
+        category = (Category) intent.getSerializableExtra(AppPreferences.CATEGORY);
+        articleName = intent.getStringExtra(AppPreferences.ARTICLE_NAME);
+        connectionsList = (RecyclerView) findViewById(R.id.recyclerConnections);
+
+        setUpArticleCore();
+    }
 
     /**
      * Load data pertaining to the selected Article, and use it in set-up processes that are common
      * to Article Activities of all Categories.
      */
-    protected void setUpArticleCore() {
-
+    private void setUpArticleCore() {
+        setAppBar();
+        bottomBar.highlightCategoryButton(this, Category.Person);
+        populateConnections();
     }
 
     /**
@@ -25,4 +76,29 @@ public abstract class ArticleActivity extends AppCompatActivity {
      */
     protected abstract int getLayoutResourceID();
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.article_menu, menu);
+        return true;
+    }
+
+    /**
+     * Set this Activity's toolbar and set the title to be the Article's name.
+     */
+    private void setAppBar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+        assert getSupportActionBar() != null;
+        getSupportActionBar().setTitle(articleName);
+    }
+
+    /**
+     * Populate the Connections RecyclerView with cards for this Article's Connections.
+     */
+    private void populateConnections() {
+        connectionsList.setLayoutManager(new LinearLayoutManager(this));
+        connectionsList.setAdapter(new ConnectionsAdapter(this, worldName, category, articleName));
+    }
 }
--- app/src/main/java/com/averi/worldscribe/activities/PersonActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/PersonActivity.java
@@ -16,47 +16,14 @@ import com.averi.worldscribe.adapters.ConnectionsAdapter;
 
 import com.averi.worldscribe.utilities.AppPreferences;
 
-public class PersonActivity extends AppCompatActivity {
-
-    private BottomBar bottomBar;
-    private String worldName;
-    private Category category;
-    private String personName;
-    private RecyclerView connectionsList;
-
+public class PersonActivity extends ArticleActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_person);
-
-        bottomBar = (BottomBar) findViewById(R.id.bottomBar);
-        Intent intent = getIntent();
-        worldName = intent.getStringExtra(AppPreferences.WORLD_NAME);
-        category = (Category) intent.getSerializableExtra(AppPreferences.CATEGORY);
-        personName = intent.getStringExtra(AppPreferences.ARTICLE_NAME);
-        connectionsList = (RecyclerView) findViewById(R.id.recyclerConnections);
-
-        setAppBar();
-        bottomBar.highlightCategoryButton(this, Category.Person);
-        populateConnections();
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.article_menu, menu);
-        return true;
-    }
-
-    private void setAppBar() {
-        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
-        setSupportActionBar(myToolbar);
-        assert getSupportActionBar() != null;
-        getSupportActionBar().setTitle(personName);
-    }
-
-    private void populateConnections() {
-        connectionsList.setLayoutManager(new LinearLayoutManager(this));
-        connectionsList.setAdapter(new ConnectionsAdapter(this, worldName, category, personName));
+    protected int getLayoutResourceID() {
+        return R.layout.activity_person;
     }
 }
