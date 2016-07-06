876719f95dc5deea7bd4b54942888e03f2afe39d
Mark Padilla <markjjpadilla@gmail.com>
2016-07-06 12:05:48 -0400

    Remove unused imports in PersonActivity
    This is because of the methods transferred to ArticleActivity.

--- app/src/main/java/com/averi/worldscribe/activities/PersonActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/PersonActivity.java
@@ -1,20 +1,10 @@
 package com.averi.worldscribe.activities;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 
 import com.averi.worldscribe.views.BottomBar;
-import com.averi.worldscribe.Category;
 import com.averi.worldscribe.R;
-import com.averi.worldscribe.adapters.ConnectionsAdapter;
-
-import com.averi.worldscribe.utilities.AppPreferences;
 
 public class PersonActivity extends ArticleActivity {
 
