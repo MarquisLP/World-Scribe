aa233363cb95b9befb5f1739d4298921dc2e734b
Mark Padilla <markjjpadilla@gmail.com>
2016-07-08 22:16:43 -0400

    Display Snippet content in SnippetActivity

--- app/src/main/java/com/averi/worldscribe/activities/SnippetActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/SnippetActivity.java
@@ -4,20 +4,34 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
+import com.averi.worldscribe.Category;
 import com.averi.worldscribe.R;
 import com.averi.worldscribe.utilities.AppPreferences;
+import com.averi.worldscribe.utilities.ExternalReader;
 
 public class SnippetActivity extends AppCompatActivity {
+
+    /**
+     * The text field for editing the Snippet's content.
+     */
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_snippet);
 
+        editText = (EditText) findViewById(R.id.editText);
+
         Intent intent = getIntent();
         String snippetName = intent.getStringExtra(AppPreferences.SNIPPET_NAME);
         setAppBarTitle(snippetName);
+        loadSnippetContent(intent.getStringExtra(AppPreferences.WORLD_NAME),
+                (Category) intent.getSerializableExtra(AppPreferences.CATEGORY),
+                intent.getStringExtra(AppPreferences.ARTICLE_NAME),
+                snippetName);
     }
 
     /**
@@ -29,5 +43,15 @@ public class SnippetActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
         assert getSupportActionBar() != null;
         getSupportActionBar().setTitle(snippetName);
+    }
+
+    /**
+     * Display the contents of the Snippet stored on file.
+     */
+    private void loadSnippetContent(String worldName, Category category, String articleName,
+                                    String snippetName) {
+        String snippetContent = ExternalReader.getSnippetText(this, worldName, category,
+                articleName, snippetName);
+        editText.setText(snippetContent);
     }
 }
