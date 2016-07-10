5d2c11df8a3a7e408d8042fdacc2f01714b6f9bc
Mark Padilla <markjjpadilla@gmail.com>
2016-07-10 11:49:41 -0400

    Make GroupActivity a subclass of ArticleActivity

--- app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
@@ -3,22 +3,40 @@ package com.averi.worldscribe.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.views.BottomBar;
 
-public class GroupActivity extends AppCompatActivity {
+public class GroupActivity extends ArticleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_group);
-
-        highlightGroupButton();
     }
 
-    private  void highlightGroupButton() {
-        ImageButton groupButton = (ImageButton) (findViewById(R.id.bottomBar).findViewById(R.id.groupsButton));
-        groupButton.setBackgroundColor(ContextCompat.getColor(this, R.color.averiBlueHighlight));
+    @Override
+    protected int getLayoutResourceID() {
+        return R.layout.activity_group;
+    }
+
+    @Override
+    protected ImageView getImageView() { return (ImageView) findViewById(R.id.imageGroup); }
+
+    @Override
+    protected BottomBar getBottomBar() {
+        return (BottomBar) findViewById(R.id.bottomBar);
+    }
+
+    @Override
+    protected RecyclerView getConnectionsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerConnections);
+    }
+
+    @Override
+    protected RecyclerView getSnippetsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerSnippets);
     }
 }
