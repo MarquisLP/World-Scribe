2fef69b46343aff7bbedadee4b109fc30ec49bd3
Mark Padilla <markjjpadilla@gmail.com>
2016-07-11 16:42:06 -0400

    Make ItemActivity a subclass of ArticleActivity

--- app/src/main/java/com/averi/worldscribe/activities/ItemActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/ItemActivity.java
@@ -3,22 +3,41 @@ package com.averi.worldscribe.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.views.BottomBar;
 
-public class ItemActivity extends AppCompatActivity {
+public class ItemActivity extends ArticleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
-
-        highlightItemsButton();
     }
 
-    private  void highlightItemsButton() {
-        ImageButton itemsButton = (ImageButton) (findViewById(R.id.bottomBar).findViewById(R.id.itemsButton));
-        itemsButton.setBackgroundColor(ContextCompat.getColor(this, R.color.averiBlueHighlight));
+    @Override
+    protected int getLayoutResourceID() {
+        return R.layout.activity_item;
     }
+
+    @Override
+    protected ImageView getImageView() { return (ImageView) findViewById(R.id.imageItem); }
+
+    @Override
+    protected BottomBar getBottomBar() {
+        return (BottomBar) findViewById(R.id.bottomBar);
+    }
+
+    @Override
+    protected RecyclerView getConnectionsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerConnections);
+    }
+
+    @Override
+    protected RecyclerView getSnippetsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerSnippets);
+    }
+
 }
