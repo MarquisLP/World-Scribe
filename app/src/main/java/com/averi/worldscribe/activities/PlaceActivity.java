05746cc3cbf2994d9551c9479926ffbbcaaf4268
Mark Padilla <markjjpadilla@gmail.com>
2016-07-11 16:23:34 -0400

    Make PlaceActivity a subclass of ArticleActivity

--- app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
@@ -3,22 +3,41 @@ package com.averi.worldscribe.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.views.BottomBar;
 
-public class PlaceActivity extends AppCompatActivity {
+public class PlaceActivity extends ArticleActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_place);
-
-        highlightPeopleButton();
     }
 
-    private  void highlightPeopleButton() {
-        ImageButton groupButton = (ImageButton) (findViewById(R.id.bottomBar).findViewById(R.id.placesButton));
-        groupButton.setBackgroundColor(ContextCompat.getColor(this, R.color.averiBlueHighlight));
+    @Override
+    protected int getLayoutResourceID() {
+        return R.layout.activity_place;
     }
+
+    @Override
+    protected ImageView getImageView() { return (ImageView) findViewById(R.id.imagePlace); }
+
+    @Override
+    protected BottomBar getBottomBar() {
+        return (BottomBar) findViewById(R.id.bottomBar);
+    }
+
+    @Override
+    protected RecyclerView getConnectionsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerConnections);
+    }
+
+    @Override
+    protected RecyclerView getSnippetsRecycler() {
+        return (RecyclerView) findViewById(R.id.recyclerSnippets);
+    }
+
 }
