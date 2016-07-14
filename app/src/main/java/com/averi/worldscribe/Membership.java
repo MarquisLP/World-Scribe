3905a13b70928e3d19331dbabbdb74b24f3a1bb7
Mark Padilla <markjjpadilla@gmail.com>
2016-07-14 19:49:54 -0400

    Create Membership data class
    This will be used to transfer data to a Person's Membership cards.

---
+++ app/src/main/java/com/averi/worldscribe/Membership.java
@@ -0,0 +1,17 @@
+package com.averi.worldscribe;
+
+/**
+ * Created by mark on 14/07/16.
+ * A class containing data on a Person's Membership within a certain Group.
+ */
+public class Membership {
+    /**
+     * The name of the Group this Membership is for.
+     */
+    public String groupName;
+    /**
+     * The role or rank the Person has within the Group.
+     * This field is optional; an empty String signifies no role or rank.
+     */
+    public String memberRole = "";
+}
