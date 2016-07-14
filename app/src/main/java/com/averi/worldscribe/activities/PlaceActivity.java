b7bbd22ec40199d47ad08c4317fc9ec5d90813c7
Mark Padilla <markjjpadilla@gmail.com>
2016-07-13 20:37:28 -0400

    Add Resident cards to PlaceActivity

--- app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
@@ -3,18 +3,26 @@ package com.averi.worldscribe.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.adapters.ResidentsAdapter;
 import com.averi.worldscribe.views.BottomBar;
 
 public class PlaceActivity extends ArticleActivity {
 
+    private RecyclerView residentsList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        residentsList = (RecyclerView) findViewById(R.id.recyclerResidents);
+
+        populateResidences();
     }
 
     @Override
@@ -38,6 +46,14 @@ public class PlaceActivity extends ArticleActivity {
     @Override
     protected RecyclerView getSnippetsRecycler() {
         return (RecyclerView) findViewById(R.id.recyclerSnippets);
+    }
+
+    /**
+     * Populate the Residents RecyclerView with cards for this Place's Residents.
+     */
+    private void populateResidences() {
+        residentsList.setLayoutManager(new LinearLayoutManager(this));
+        residentsList.setAdapter(new ResidentsAdapter(this, getWorldName(), getArticleName()));
     }
 
 }
