c86a4ec5582eab26b770e79b31d2ea257e5beb30
Mark Padilla <markjjpadilla@gmail.com>
2016-07-17 19:06:20 -0400

    Display Members list in GroupActivity

--- app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
@@ -3,18 +3,26 @@ package com.averi.worldscribe.activities;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.adapters.MembersAdapter;
 import com.averi.worldscribe.views.BottomBar;
 
 public class GroupActivity extends ArticleActivity {
 
+    private RecyclerView membersList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        membersList = (RecyclerView) findViewById(R.id.recyclerMembers);
+
+        populateMembers();
     }
 
     @Override
@@ -38,5 +46,10 @@ public class GroupActivity extends ArticleActivity {
     @Override
     protected RecyclerView getSnippetsRecycler() {
         return (RecyclerView) findViewById(R.id.recyclerSnippets);
+    }
+
+    private void populateMembers() {
+        membersList.setLayoutManager(new LinearLayoutManager(this));
+        membersList.setAdapter(new MembersAdapter(this, getWorldName(), getArticleName()));
     }
 }
