022bf711c8ef8973866dffd6242dd8b51dfc136d
Mark Padilla <markjjpadilla@gmail.com>
2016-07-18 12:53:19 -0400

    Load text field data in GroupActivity

--- app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/GroupActivity.java
@@ -1,28 +1,39 @@
 package com.averi.worldscribe.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.widget.ImageButton;
+import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
 import com.averi.worldscribe.adapters.MembersAdapter;
+import com.averi.worldscribe.utilities.ExternalReader;
 import com.averi.worldscribe.views.BottomBar;
 
 public class GroupActivity extends ArticleActivity {
 
     private RecyclerView membersList;
+    private EditText mandateField;
+    private EditText historyField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         membersList = (RecyclerView) findViewById(R.id.recyclerMembers);
+        mandateField = (EditText) findViewById(R.id.editMandate);
+        historyField = (EditText) findViewById(R.id.editHistory);
 
         populateMembers();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        loadTextFields();
     }
 
     @Override
@@ -51,5 +62,16 @@ public class GroupActivity extends ArticleActivity {
     private void populateMembers() {
         membersList.setLayoutManager(new LinearLayoutManager(this));
         membersList.setAdapter(new MembersAdapter(this, getWorldName(), getArticleName()));
+    }
+
+    /**
+     * Load data for the Group's text fields and display them.
+     */
+    private void loadTextFields() {
+        Resources resources = getResources();
+        mandateField.setText(ExternalReader.getArticleTextFieldData(this, getWorldName(),
+                getCategory(), getArticleName(), resources.getString(R.string.mandateText)));
+        historyField.setText(ExternalReader.getArticleTextFieldData(this, getWorldName(),
+                getCategory(), getArticleName(), resources.getString(R.string.historyHint)));
     }
 }
