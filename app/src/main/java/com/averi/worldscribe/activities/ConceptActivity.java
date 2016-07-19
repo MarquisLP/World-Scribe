af00fba19c4d489262ffb85ff6f10208652d265f
Mark Padilla <markjjpadilla@gmail.com>
2016-07-19 12:12:36 -0400

    Load text field data in ConceptActivity

--- app/src/main/java/com/averi/worldscribe/activities/ConceptActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/ConceptActivity.java
@@ -1,20 +1,34 @@
 package com.averi.worldscribe.activities;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.utilities.ExternalReader;
 import com.averi.worldscribe.views.BottomBar;
 
 public class ConceptActivity extends ArticleActivity {
 
+    private EditText descriptionField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        descriptionField = (EditText) findViewById(R.id.editDescription);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        loadTextFields();
     }
 
     @Override
@@ -38,6 +52,15 @@ public class ConceptActivity extends ArticleActivity {
     @Override
     protected RecyclerView getSnippetsRecycler() {
         return (RecyclerView) findViewById(R.id.recyclerSnippets);
+    }
+
+    /**
+     * Load data for the Concept's text fields and display them.
+     */
+    private void loadTextFields() {
+        Resources resources = getResources();
+        descriptionField.setText(ExternalReader.getArticleTextFieldData(this, getWorldName(),
+                getCategory(), getArticleName(), resources.getString(R.string.descriptionHint)));
     }
 
 }
