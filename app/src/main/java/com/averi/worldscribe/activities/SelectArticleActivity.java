56535c718f8e8f15b07a083c525e18df5422d3a6
Mark Padilla <markjjpadilla@gmail.com>
2016-07-29 17:36:47 -0400

    Create SelectArticleActivity
    This Activity will be used to select the connected Article when creating
    or editing a Connection, Resident/Residence, or Member/Membership.

---
+++ app/src/main/java/com/averi/worldscribe/activities/SelectArticleActivity.java
@@ -0,0 +1,15 @@
+package com.averi.worldscribe.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.averi.worldscribe.R;
+
+public class SelectArticleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_article);
+    }
+}
