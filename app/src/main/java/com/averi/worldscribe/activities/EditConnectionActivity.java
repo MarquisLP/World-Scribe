65286a3017d0534a8a68c9f544b3d7a95c8a191f
Mark Padilla <markjjpadilla@gmail.com>
2016-07-30 18:28:24 -0400

    Add SAVE button to EditConnectionActivity app bar

--- app/src/main/java/com/averi/worldscribe/activities/EditConnectionActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/EditConnectionActivity.java
@@ -2,6 +2,7 @@ package com.averi.worldscribe.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 import com.averi.worldscribe.R;
 
@@ -11,5 +12,11 @@ public class EditConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_connection);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save_edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
