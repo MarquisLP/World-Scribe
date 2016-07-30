ba6a28b39d4700386705a6e679985013bb54785b
Mark Padilla <markjjpadilla@gmail.com>
2016-07-30 19:18:52 -0400

    Set up app bar in EditConnectionActivity

--- app/src/main/java/com/averi/worldscribe/activities/EditConnectionActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/EditConnectionActivity.java
@@ -2,6 +2,7 @@ package com.averi.worldscribe.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 
 import com.averi.worldscribe.R;
@@ -12,6 +13,8 @@ public class EditConnectionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_connection);
+
+        setAppBar();
     }
 
     @Override
@@ -19,4 +22,16 @@ public class EditConnectionActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.save_edit_menu, menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    /**
+     * Set up this Activity's app bar.
+     */
+    private void setAppBar() {
+        Toolbar appBar = (Toolbar) findViewById(R.id.my_toolbar);
+        if (appBar != null) {
+            appBar.setTitle(R.string.editConnectionTitle);
+            setSupportActionBar(appBar);
+        }
+    }
+
 }
