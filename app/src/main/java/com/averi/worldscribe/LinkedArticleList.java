0e7ee75536d3afd5e55c5417b9afe470939bb903
Mark Padilla <markjjpadilla@gmail.com>
2016-07-31 17:17:16 -0400

    Created LinkedArticleList class
    An object of this type will be passed to SelectArticleActivity to
    determine which Articles are already linked and, thus, should not be
    shown so that pre-existing links aren't overwritten during creation of
    new Connections/Memberships/Residences.

---
+++ app/src/main/java/com/averi/worldscribe/LinkedArticleList.java
@@ -0,0 +1,70 @@
+package com.averi.worldscribe;
+
+import java.util.HashSet;
+
+/**
+ * <p>
+ * Created by mark on 31/07/16.
+ * </p>
+ *
+ * <p>
+ * Contains a list of all of the Articles that are linked to a certain Article through one
+ * of the three types of links (Connection, Membership, or Residence).
+ * </p>
+ */
+public class LinkedArticleList {
+
+    private HashSet<String> personNames;
+    private HashSet<String> groupNames;
+    private HashSet<String> placeNames;
+    private HashSet<String> itemNames;
+    private HashSet<String> conceptNames;
+
+    /**
+     * Adds an Article to this list.
+     * @param category The {@link Category} of the Article.
+     * @param name The name of the Article.
+     */
+    public void addArticle(Category category, String name) {
+        switch (category) {
+            case Person:
+                personNames.add(name);
+                break;
+            case Group:
+                groupNames.add(name);
+                break;
+            case Place:
+                placeNames.add(name);
+                break;
+            case Item:
+                itemNames.add(name);
+                break;
+            case Concept:
+            default:
+                conceptNames.add(name);
+                break;
+        }
+    }
+
+    /**
+     * Gets all Article links within a certain Category.
+     * @param category The {@link Category} of Articles to return.
+     * @return A HashSet containing all Article links of the specified Category.
+     */
+    public HashSet<String> getAllLinksInCategory(Category category) {
+        switch (category) {
+            case Person:
+                return personNames;
+            case Group:
+                return groupNames;
+            case Place:
+                return placeNames;
+            case Item:
+                return itemNames;
+            case Concept:
+            default:
+                return conceptNames;
+        }
+    }
+
+}
