ee22ef1dac8e3b566e604299a722590db9d58270
Mark Padilla <markjjpadilla@gmail.com>
2016-08-04 13:14:10 -0400

    Display text fields in EditMembershipActivity

--- app/src/main/java/com/averi/worldscribe/activities/EditMembershipActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/EditMembershipActivity.java
@@ -3,6 +3,8 @@ package com.averi.worldscribe.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.averi.worldscribe.Membership;
 import com.averi.worldscribe.R;
@@ -12,12 +14,34 @@ public class EditMembershipActivity extends AppCompatActivity {
 
     private Membership membership;
 
+    private TextView groupNameText;
+    private TextView memberNameText;
+    private EditText memberRoleField;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_membership);
 
+        groupNameText = (TextView) findViewById(R.id.groupName);
+        memberNameText = (TextView) findViewById(R.id.memberName);
+        memberRoleField = (EditText) findViewById(R.id.memberRoleField);
+
         Intent startupIntent = getIntent();
         membership = (Membership) startupIntent.getSerializableExtra(IntentFields.MEMBERSHIP);
+
+        setTextFields();
+    }
+
+    /**
+     * Displays the Group name, member name, and member's role (if it exists) when this Activity is
+     * created.
+     */
+    private void setTextFields() {
+        groupNameText.setText(membership.groupName);
+        memberNameText.setText(membership.memberName);
+        if (membership.memberRole != null) {
+            memberRoleField.setText(membership.memberRole);
+        }
     }
 }
