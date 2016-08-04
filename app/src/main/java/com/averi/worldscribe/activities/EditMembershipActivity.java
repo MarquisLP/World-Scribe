bf171645e94494d0d950cb88a77ac60d7f2b7a1f
Mark Padilla <markjjpadilla@gmail.com>
2016-08-04 13:57:01 -0400

    Setup app bar in EditConnectionActivity

--- app/src/main/java/com/averi/worldscribe/activities/EditMembershipActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/EditMembershipActivity.java
@@ -3,6 +3,8 @@ package com.averi.worldscribe.activities;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -30,7 +32,14 @@ public class EditMembershipActivity extends AppCompatActivity {
         Intent startupIntent = getIntent();
         membership = (Membership) startupIntent.getSerializableExtra(IntentFields.MEMBERSHIP);
 
+        setAppBar();
         setTextFields();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.save_edit_menu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     /**
@@ -42,6 +51,17 @@ public class EditMembershipActivity extends AppCompatActivity {
         memberNameText.setText(membership.memberName);
         if (membership.memberRole != null) {
             memberRoleField.setText(membership.memberRole);
+        }
+    }
+
+    /**
+     * Set up this Activity's app bar.
+     */
+    private void setAppBar() {
+        Toolbar appBar = (Toolbar) findViewById(R.id.my_toolbar);
+        if (appBar != null) {
+            appBar.setTitle(R.string.editMembershipTitle);
+            setSupportActionBar(appBar);
         }
     }
 }
