ae9098c06ffe7b3e1c1e9659de118d35b9da7902
Mark Padilla <markjjpadilla@gmail.com>
2016-08-05 12:42:53 -0400

    Create Residence class
    Though the ResidencesAdapter and ResidentsAdapter only really need a
    list of Strings, using lists with a dedicated data class has some
    benefits:
    
    * A Residence object can be passed to ExternalWriter and ExternalDeleter
      for saving/deletion with all of the needed info, significantly cutting
      down on the parameters that need to be passed.
    * The Residence class maintains consistency with the Connection and
      Membership data classes.
    * If new fields ever need to be added to Residences in the future, they
      can be added to the class without needing any major changes to the
      Adapters or ExternalReader/Writer/Deleter.

---
+++ app/src/main/java/com/averi/worldscribe/Residence.java
@@ -0,0 +1,20 @@
+package com.averi.worldscribe;
+
+/**
+ * Created by mark on 05/08/16.
+ * A class containing data on a Person's residence within a certain Place.
+ */
+public class Residence {
+    /**
+     * The name of the World containing the Place and resident.
+     */
+    public String worldName;
+    /**
+     * The name of the Place of residence.
+     */
+    public String placeName;
+    /**
+     * The name of the Person taking residence.
+     */
+    public String residentName;
+}
