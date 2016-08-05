88ccf8f3b37736110e2bcf9d0c11be7bca458586
Mark Padilla <markjjpadilla@gmail.com>
2016-08-05 13:57:27 -0400

    Open article selection when adding new Resident

--- app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/PlaceActivity.java
@@ -1,11 +1,13 @@
 package com.averi.worldscribe.activities;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -14,23 +16,39 @@ import android.widget.ImageView;
 import com.averi.worldscribe.ArticleTextField;
 import com.averi.worldscribe.Category;
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.adapters.ResidencesAdapter;
 import com.averi.worldscribe.adapters.ResidentsAdapter;
 import com.averi.worldscribe.utilities.ExternalReader;
+import com.averi.worldscribe.utilities.IntentFields;
 import com.averi.worldscribe.views.BottomBar;
 
 import java.util.ArrayList;
 
 public class PlaceActivity extends ArticleActivity {
 
+    /**
+     * The request code for a new Resident within this Place.
+     */
+    public static final int RESULT_NEW_RESIDENT = 300;
+
     private RecyclerView residentsList;
+    private Button addResidentButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         residentsList = (RecyclerView) findViewById(R.id.recyclerResidents);
+        addResidentButton = (Button) findViewById(R.id.buttonAddResident);
 
         populateResidences();
+
+        addResidentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addResident();
+            }
+        });
     }
 
     @Override
@@ -82,6 +100,23 @@ public class PlaceActivity extends ArticleActivity {
     private void populateResidences() {
         residentsList.setLayoutManager(new LinearLayoutManager(this));
         residentsList.setAdapter(new ResidentsAdapter(this, getWorldName(), getArticleName()));
+    }
+
+    /**
+     * Opens SelectArticleActivity so the user can select the Person for a new
+     * {@link com.averi.worldscribe.Residence Residence} within this Place.
+     */
+    private void addResident() {
+        Intent selectGroupIntent = new Intent(this, SelectArticleActivity.class);
+        ResidentsAdapter residentsAdapter = (ResidentsAdapter) residentsList.getAdapter();
+
+        selectGroupIntent.putExtra(IntentFields.WORLD_NAME, getWorldName());
+        selectGroupIntent.putExtra(IntentFields.CATEGORY, Category.Person);
+        selectGroupIntent.putExtra(IntentFields.MAIN_ARTICLE_CATEGORY, Category.Place);
+        selectGroupIntent.putExtra(IntentFields.MAIN_ARTICLE_NAME, getArticleName());
+        selectGroupIntent.putExtra(IntentFields.EXISTING_LINKS,
+                residentsAdapter.getLinkedArticleList());
+        startActivityForResult(selectGroupIntent, RESULT_NEW_RESIDENT);
     }
 
 }
