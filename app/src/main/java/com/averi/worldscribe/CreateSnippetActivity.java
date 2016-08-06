45aa11eb4f2ce4b25356971b3913172da97995ec
Mark Padilla <markjjpadilla@gmail.com>
2016-08-06 16:07:38 -0400

    Prevent creating Snippets with no name

--- app/src/main/java/com/averi/worldscribe/CreateSnippetActivity.java
+++ app/src/main/java/com/averi/worldscribe/CreateSnippetActivity.java
@@ -4,7 +4,11 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.averi.worldscribe.utilities.IntentFields;
 
@@ -17,6 +21,9 @@ public class CreateSnippetActivity extends AppCompatActivity {
     private Category articleCategory;
     private String articleName;
 
+    private EditText nameField;
+    private Button createButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +34,11 @@ public class CreateSnippetActivity extends AppCompatActivity {
         articleCategory = (Category) startupIntent.getSerializableExtra(IntentFields.CATEGORY);
         articleName = startupIntent.getStringExtra(IntentFields.ARTICLE_NAME);
 
+        nameField = (EditText) findViewById(R.id.nameField);
+        createButton = (Button) findViewById(R.id.create);
+
         setAppBar();
+        setNameFieldWatcher();
     }
 
     /**
@@ -41,7 +52,50 @@ public class CreateSnippetActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Sets a TextWatcher to monitor the contents of the name text field, and activate or
+     * deactivate the Create button as appropriate.
+     */
+    private void setNameFieldWatcher() {
+        nameField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                activateCreateButtonWhenNameIsNonEmpty();
+            }
+        });
+    }
+
+    /**
+     * Allows the "Create" button to respond to clicks if and only if the name field is non-empty.
+     * This is to ensure that every Snippet has a name.
+     */
+    private void activateCreateButtonWhenNameIsNonEmpty() {
+        if (nameIsEmpty()) {
+            createButton.setEnabled(false);
+        } else {
+            createButton.setEnabled(true);
+        }
+    }
+
+    /**
+     * @return True if there is no text entered into the name text field.
+     */
+    private boolean nameIsEmpty() {
+        return (nameField.getText().toString().isEmpty());
+    }
+
     public void clickCreate(View view) {
 
     }
+
 }
