9c8ad3d36d6a0e2f7612df9e5a811678f7989d97
Mark Padilla <markjjpadilla@gmail.com>
2016-08-06 16:48:47 -0400

    Increase maximum lines for Snackbar messages

--- app/src/main/java/com/averi/worldscribe/utilities/ErrorMessager.java
+++ app/src/main/java/com/averi/worldscribe/utilities/ErrorMessager.java
@@ -12,6 +12,11 @@ import com.averi.worldscribe.R;
  * Created by mark on 20/06/16.
  */
 public class ErrorMessager {
+    /**
+     * The maximum number of lines that can be displayed by a Snackbar message.
+     */
+    public static final int SNACKBAR_MAX_LINES = 5;
+
     public static void showSnackbarMessage(Context context, View parentView, String message) {
         Snackbar snackbar = Snackbar
                 .make(parentView, message, Snackbar.LENGTH_LONG);
@@ -19,6 +24,7 @@ public class ErrorMessager {
         snackbarView.setBackgroundColor(AttributeGetter.getColorAttribute(context, R.attr.colorPrimary));
         TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
         textView.setTextColor(Color.WHITE);
+        textView.setMaxLines(SNACKBAR_MAX_LINES);
         snackbar.show();
     }
 }
