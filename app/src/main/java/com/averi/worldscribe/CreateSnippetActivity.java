3ffe03ab2d0a5c3df249aedae951a86dea26579f
Mark Padilla <markjjpadilla@gmail.com>
2016-08-06 15:24:34 -0400

    Create CreateSnippetActivity

---
+++ app/src/main/java/com/averi/worldscribe/CreateSnippetActivity.java
@@ -0,0 +1,21 @@
+package com.averi.worldscribe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * The Activity where the user names the new Snippet during Snippet creation.
+ */
+public class CreateSnippetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_snippet);
+    }
+
+    public void clickCreate(View view) {
+
+    }
+}
