3fd2a958af45e7b4c14132ad242820b0072991d5
Mark Padilla <markjjpadilla@gmail.com>
2016-08-15 19:28:14 -0400

    Setup app bar in SettingsActivity

--- app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
@@ -2,6 +2,7 @@ package com.averi.worldscribe.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.averi.worldscribe.R;
 
@@ -11,6 +12,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        setAppBar();
+    }
+
+    /**
+     * Setup this Activity's app bar.
+     */
+    private void setAppBar() {
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        assert myToolbar != null;
+        myToolbar.setTitle(R.string.settingsTitle);
+        setSupportActionBar(myToolbar);
     }
 
 }
