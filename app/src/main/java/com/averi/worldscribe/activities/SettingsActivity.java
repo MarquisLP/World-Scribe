9722c95c3bb49060dd556154374d7f63be0040fb
Mark Padilla <markjjpadilla@gmail.com>
2016-08-15 21:11:09 -0400

    Set adapter for select app theme spinner

--- app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
@@ -3,17 +3,24 @@ package com.averi.worldscribe.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.widget.Spinner;
 
 import com.averi.worldscribe.R;
+import com.averi.worldscribe.adapters.AppThemeArrayAdapter;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private Spinner appThemeSpinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        appThemeSpinner = (Spinner) findViewById(R.id.themeSelector);
+
         setAppBar();
+        populateAppThemeSpinner();
     }
 
     /**
@@ -24,6 +31,16 @@ public class SettingsActivity extends AppCompatActivity {
         assert myToolbar != null;
         myToolbar.setTitle(R.string.settingsTitle);
         setSupportActionBar(myToolbar);
+    }
+
+    /**
+     * Populate the App Theme spinner with all available app themes.
+     */
+    private void populateAppThemeSpinner() {
+        Integer allThemes[] = {R.style.AppTheme, R.style.SunsetOrange, R.style.EnchantedGreen,
+            R.style.UbePurple, R.style.LovelyRed};
+        AppThemeArrayAdapter adapter = new AppThemeArrayAdapter(this, allThemes);
+        appThemeSpinner.setAdapter(adapter);
     }
 
 }
