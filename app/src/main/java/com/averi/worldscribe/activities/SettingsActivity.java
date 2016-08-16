ddb25dbba6254a8c84ce85107557681eddf871e1
Mark Padilla <markjjpadilla@gmail.com>
2016-08-15 22:14:39 -0400

    Save selected theme in SettingsActivity

--- app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
+++ app/src/main/java/com/averi/worldscribe/activities/SettingsActivity.java
@@ -1,15 +1,25 @@
 package com.averi.worldscribe.activities;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Spinner;
 
 import com.averi.worldscribe.R;
 import com.averi.worldscribe.adapters.AppThemeArrayAdapter;
+import com.averi.worldscribe.utilities.AppPreferences;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends ThemedActivity {
+
+    public static final int SKY_BLUE = 0;
+    public static final int SUNSET_ORANGE = 1;
+    public static final int ENCHANTED_GREEN = 2;
+    public static final int UBE_PURPLE = 3;
+    public static final int LOVELY_RED = 4;
 
     private Spinner appThemeSpinner;
 
@@ -30,6 +40,18 @@ public class SettingsActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.saveEditItem:
+                saveAppSettings();
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
     /**
      * Setup this Activity's app bar.
      */
@@ -48,6 +70,42 @@ public class SettingsActivity extends AppCompatActivity {
             R.style.UbePurple, R.style.LovelyRed};
         AppThemeArrayAdapter adapter = new AppThemeArrayAdapter(this, allThemes);
         appThemeSpinner.setAdapter(adapter);
+    }
+
+    /**
+     * Saves the app settings that are set in this Activity.
+     */
+    private void saveAppSettings() {
+        SharedPreferences preferences = getSharedPreferences(AppPreferences.PREFERENCES_FILE_NAME,
+                MODE_PRIVATE);
+        preferences.edit().putInt(AppPreferences.APP_THEME, getSelectedAppTheme()).apply();
+    }
+
+    /**
+     * @return The ID of the theme selected by the user.
+     */
+    private int getSelectedAppTheme() {
+        int themeID;
+
+        switch (appThemeSpinner.getSelectedItemPosition()) {
+            case SKY_BLUE:
+            default:
+                themeID = R.style.AppTheme;
+                break;
+            case SUNSET_ORANGE:
+                themeID = R.style.SunsetOrange;
+                break;
+            case ENCHANTED_GREEN:
+                themeID = R.style.EnchantedGreen;
+                break;
+            case UBE_PURPLE:
+                themeID = R.style.UbePurple;
+                break;
+            case LOVELY_RED:
+                themeID = R.style.LovelyRed;
+        }
+
+        return themeID;
     }
 
 }
