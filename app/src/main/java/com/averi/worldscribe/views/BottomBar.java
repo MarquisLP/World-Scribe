e2e7943ceef86aeeb506d80d0dc46fca4a075ba8
Mark Padilla <markjjpadilla@gmail.com>
2016-08-29 19:50:18 -0400

    Add slide-in/out animations to BottomBar

--- app/src/main/java/com/averi/worldscribe/views/BottomBar.java
+++ app/src/main/java/com/averi/worldscribe/views/BottomBar.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
@@ -18,6 +20,9 @@ import com.averi.worldscribe.utilities.AttributeGetter;
 
 public class BottomBar extends RelativeLayout {
 
+    private Animation slideOutAnimation;
+    private Animation slideInAnimation;
+
     private LayoutInflater inflater;
     private BottomBarActivity activity;
 
@@ -28,6 +33,9 @@ public class BottomBar extends RelativeLayout {
 
             inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             inflater.inflate(R.layout.activity_bottom_bar, this, true);
+
+            slideOutAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_out_bottom);
+            slideInAnimation = AnimationUtils.loadAnimation(context, R.anim.slide_in_from_bottom);
 
             this.findViewById(R.id.peopleButton).setOnClickListener(peopleOnClickListener);
             this.findViewById(R.id.groupsButton).setOnClickListener(groupsOnClickListener);
@@ -105,6 +113,22 @@ public class BottomBar extends RelativeLayout {
         (findViewById(R.id.placesButton)).setBackgroundColor(normalButtonColor);
         (findViewById(R.id.itemsButton)).setBackgroundColor(normalButtonColor);
         (findViewById(R.id.conceptsButton)).setBackgroundColor(normalButtonColor);
+    }
+
+    /**
+     * Translates the bar down off the bottom edge of the screen so that it is not visible.
+     */
+    public void slideOut() {
+        this.startAnimation(slideOutAnimation);
+        this.setVisibility(GONE);
+    }
+
+    /**
+     * Translates the bar up from the bottom edge of the screen so that it is visible.
+     */
+    public void slideIn() {
+        this.setVisibility(VISIBLE);
+        this.startAnimation(slideInAnimation);
     }
 
 }
