762ee1a6c9b8ec5b2a49db349816f4f9c02bba5d
Mark Padilla <markjjpadilla@gmail.com>
2016-12-24 14:36:13 -0500

    Create UploadToDropboxTask

---
+++ app/src/main/java/com/averi/worldscribe/dropbox/UploadToDropboxTask.java
@@ -0,0 +1,69 @@
+package com.averi.worldscribe.dropbox;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import com.averi.worldscribe.R;
+import com.dropbox.core.DbxException;
+import com.dropbox.core.v2.DbxClientV2;
+import com.dropbox.core.v2.files.WriteMode;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by mark on 24/12/16.
+ *
+ * <p>
+ *     Code copied and modified from SitePoint:<br>
+ *     https://www.sitepoint.com/adding-the-dropbox-api-to-an-android-app/
+ * </p>
+ *
+ * <p>
+ *     This AsyncTask handles file uploading to a Dropbox account. A Toast with an appropriate
+ *     message is displayed at the end of file operations, depending on whether the upload was
+ *     successful or not.
+ * </p>
+ */
+
+public class UploadToDropboxTask extends AsyncTask {
+    private DbxClientV2 dbxClient;
+    private File file;
+    private Context context;
+    private boolean uploadSuccessful = true;
+
+    UploadToDropboxTask(DbxClientV2 dbxClient, File file, Context context) {
+        this.dbxClient = dbxClient;
+        this.file = file;
+        this.context = context;
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        try {
+            InputStream inputStream = new FileInputStream(file);
+            dbxClient.files().uploadBuilder("/" + file.getName())
+                    .withMode(WriteMode.OVERWRITE)
+                    .uploadAndFinish(inputStream);
+        } catch (DbxException | IOException e) {
+            uploadSuccessful = false;
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+
+        if (uploadSuccessful) {
+            Toast.makeText(context, context.getString(R.string.dropboxUploadSuccess),
+                    Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(context, context.getString(R.string.dropboxUploadFailure),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+}
